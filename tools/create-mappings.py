650a76cc913d9fa9a4f8234a541950ece0915e66
Christopher Pearce <christopher.pearce@outlook.co.nz>
2017-05-28 18:00:40 +1200

    allowing moving the map around

--- tools/create-mappings.py
+++ tools/create-mappings.py
@@ -1,9 +1,10 @@
 import pygame
-ORANGE = (255,155,0)
-RED = (255,0,0)
+clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((1920,1080))
 done = False
+max_y_margin = 108
+max_x_margin = 192
 
 # load up the images
 game_board_image = pygame.image.load("../data/GOTv2.0.jpg")
@@ -21,20 +22,30 @@ start_coordinates = None
 
 
 def holding():
-    global held,CameraX,CameraY,start_coordinates
-    if held:
-        coordinates = pygame.mouse.get_pos()
+    global CameraX,CameraY,start_coordinates
+    coordinates = pygame.mouse.get_pos()
 
-        CameraX += (start_coordinates[0] - coordinates[0])*1.5
-        CameraY += (start_coordinates[1] - coordinates[1])*1.5
+    CameraX += (start_coordinates[0] - coordinates[0])*1.5
+    CameraY += (start_coordinates[1] - coordinates[1])*1.5
 
-        start_coordinates = coordinates
+    start_coordinates = coordinates
+
+
+def constrain_camera():
+    global CameraX,CameraY
+    CameraX = max(CameraX,-max_x_margin)
+    CameraY = max(CameraY,-max_y_margin)
+
+    print(screen.get_width())
+    CameraX = min(CameraX,current_background_image.get_width() - screen.get_width() + max_x_margin)
+    CameraY = min(CameraY,current_background_image.get_height() - screen.get_height() + max_y_margin)
 
 while not done:
     screen.fill((0,0,0))
 
     if held:
         holding()
+        constrain_camera()
 
 
     for event in pygame.event.get():
@@ -45,9 +56,11 @@ while not done:
                 start_coordinates = pygame.mouse.get_pos()
             elif event.dict['button'] == 4: # mouse wheel up
                 current_background_image = og_background
+                constrain_camera()
                 # image_used = pygame.transform.rotozoom(background_image,0,transform_amount)
             elif event.dict['button'] == 5: # mouse wheel down
                 current_background_image = half_background
+                constrain_camera()
                 # transform_amount -= 0.05
                 # image_used = pygame.transform.rotozoom(background_image,0,transform_amount)
 
@@ -71,3 +84,4 @@ while not done:
     # refresh screen
 
     pygame.display.flip()
+    clock.tick(60)
