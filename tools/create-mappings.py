06a1ce77bed50f2c494ef4c05b84c74a6acfc39f
Christopher Pearce <christopher.pearce@outlook.co.nz>
2017-05-28 21:31:20 +1200

    Allowing placement of tiles, correct zooming according to mouse

--- tools/create-mappings.py
+++ tools/create-mappings.py
@@ -2,17 +2,30 @@ import pygame
 clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((1920,1080))
-done = False
+
 max_y_margin = 108
 max_x_margin = 192
 
 # load up the images
 game_board_image = pygame.image.load("../data/GOTv2.0.jpg")
 
+# Load the image
+move_tokens = pygame.image.load("../data/Order Tokens.png")
+
+width, height = move_tokens.get_width() / 5, move_tokens.get_height()
+
+tokens = [(i*width, 0, width, height) for i in range(0,5)]
+
+
+
+placed_tokens = []
+
+
 # and the surfaces
 og_background = pygame.Surface.convert(game_board_image)
 half_background = pygame.transform.rotozoom(og_background, 0, 0.5)
-current_background_image = og_background
+TRANSFORM_AMOUNT = 1
+BACKGROUND = og_background
 
 CameraX = 0
 CameraY = 0
@@ -30,39 +43,72 @@ def holding():
 
     start_coordinates = coordinates
 
-
 def constrain_camera():
     global CameraX,CameraY
     CameraX = max(CameraX,-max_x_margin)
     CameraY = max(CameraY,-max_y_margin)
 
-    print(screen.get_width())
-    CameraX = min(CameraX,current_background_image.get_width() - screen.get_width() + max_x_margin)
-    CameraY = min(CameraY,current_background_image.get_height() - screen.get_height() + max_y_margin)
+    CameraX = min(CameraX, BACKGROUND.get_width() - screen.get_width() + max_x_margin)
+    CameraY = min(CameraY, BACKGROUND.get_height() - screen.get_height() + max_y_margin)
 
-while not done:
+
+def get_relative_mouse_position_on_map():
+    coordinates = pygame.mouse.get_pos()
+    return ((CameraX + coordinates[0])/BACKGROUND.get_width(),
+            (CameraY + coordinates[1])/BACKGROUND.get_height())
+
+def turn_relative_into_absolute_position(previous_position):
+    return (previous_position[0] * BACKGROUND.get_width() - CameraX,
+            previous_position[1] * BACKGROUND.get_height() - CameraY)
+
+
+def change_background(background):
+    global BACKGROUND, CameraX, CameraY
+
+    coordinates = pygame.mouse.get_pos()
+    pos_relative_to_map = get_relative_mouse_position_on_map()
+
+    # pos_relative_to_screen = (coordinates[0] / screen.get_width(), coordinates[1] / screen.get_height())
+
+    # x = CameraX # basically pretend that this is where the camera is located along the map
+    # y = CameraY # basically pretend that this is where the camera is located along the map
+
+    BACKGROUND = background
+
+    # the mouse was at 0.6 WIDTH when it was at 600 px
+    # the mouse is still at 600 px, but we need to set the new ABSOLUTE position to 0.6 WIDTH as well.
+
+    CameraX = pos_relative_to_map[0] * BACKGROUND.get_width() - min(max(coordinates[0], screen.get_width()*0.25),screen.get_width()*0.75)
+    CameraY = pos_relative_to_map[1] * BACKGROUND.get_height() - min(max(coordinates[1], screen.get_height()*0.25),screen.get_height()*0.75)
+
+
+GAME_RUNNING = True
+while GAME_RUNNING:
+    # fill up the screen to prevent multiple images being printed to screen
     screen.fill((0,0,0))
 
+    # check to see if the mouse is being held (so we can move the map around)
     if held:
         holding()
-        constrain_camera()
 
 
+    # run through the events
     for event in pygame.event.get():
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.dict['button'] == 1: # left mouse button pressed
                 held = True
                 start_coordinates = pygame.mouse.get_pos()
+            elif event.dict['button'] == 3: # right mouse button pressed
+                placed_tokens.append(get_relative_mouse_position_on_map())
             elif event.dict['button'] == 4: # mouse wheel up
-                current_background_image = og_background
-                constrain_camera()
-                # image_used = pygame.transform.rotozoom(background_image,0,transform_amount)
+                if BACKGROUND is not og_background:
+                    TRANSFORM_AMOUNT = 1
+                    change_background(og_background)
             elif event.dict['button'] == 5: # mouse wheel down
-                current_background_image = half_background
-                constrain_camera()
-                # transform_amount -= 0.05
-                # image_used = pygame.transform.rotozoom(background_image,0,transform_amount)
+                if BACKGROUND is not half_background:
+                    TRANSFORM_AMOUNT = 0.5
+                    change_background(half_background)
 
 
         if event.type == pygame.MOUSEBUTTONUP:
@@ -75,13 +121,26 @@ while not done:
 
 
         if event.type == pygame.QUIT:
-            done = True
-    # events
+            GAME_RUNNING = False
 
 
-    screen.blit(current_background_image, (0 - CameraX, 0 - CameraY))
+    # reconstrain the camera so it doesn't ever leave the map
+    constrain_camera()
+
+    # draw the map onto the screen
+    screen.blit(BACKGROUND, (0 - CameraX, 0 - CameraY))
+
+    # draw rectangles
+
+    for i in range(0, len(placed_tokens)):
+        rectum = placed_tokens[i]
+        new_pos = turn_relative_into_absolute_position(rectum)
+        # pygame.draw.rect(screen, (255, 0, 255), (new_pos[0] - 40*TRANSFORM_AMOUNT, new_pos[1] - 40*TRANSFORM_AMOUNT, 80*TRANSFORM_AMOUNT, 80*TRANSFORM_AMOUNT))
+
+        # Blit second half
+        screen.blit(move_tokens, new_pos, tokens[i % 5])
+
 
     # refresh screen
-
     pygame.display.flip()
     clock.tick(60)
